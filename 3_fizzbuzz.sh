6596ddf2e661ba2159304f5b7d820f324edf1065
tomasevicnikola <ntomasevic@griddynamics.com>
2026-04-01 15:16:13 +0200

    feat: add fizzbuzz bash script

---
+++ 3_fizzbuzz.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#This script prints numbers from 1 to 100 using fizzbuzz rules
+
+for((i=1;i<=100;i++)); do
+    if (( i % 3 == 0 && i % 5 == 0 )); then
+        echo "FizzBuzz"
+    elif (( i % 3 == 0 )); then
+        echo "Fizz"
+    elif (( i % 5 == 0 )); then
+        echo "Buzz"
+    else
+        echo "$i"
+    fi
+done
+
